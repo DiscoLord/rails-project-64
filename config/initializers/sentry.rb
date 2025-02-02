2f4249034f97337fa4a243a68b7b37a5cd1e35cc
Boris Nikolaev <boris28596@gmail.com>
2025-02-02 12:32:32 +0100

    Added sentry

---
+++ config/initializers/sentry.rb
@@ -0,0 +1,14 @@
+Sentry.init do |config|
+  config.dsn = 'https://2edf40881af3424acd0bf1d9b9543287@o4508749332283392.ingest.de.sentry.io/4508749335363664'
+
+  # get breadcrumbs from logs
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+
+  # enable tracing
+  # we recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
+
+  # enable profiling
+  # this is relative to traces_sample_rate
+  config.profiles_sample_rate = 1.0
+end
