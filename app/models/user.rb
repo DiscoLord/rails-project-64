8e93ccdcec5b709d9f133831a49df166475e0200
Boris Nikolaev <boris28596@gmail.com>
2025-02-08 19:40:48 +0100

    Added proper user like linking

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@
 class User < ApplicationRecord
   has_many :posts, foreign_key: 'creator_id', dependent: :destroy, inverse_of: :creator
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :likes, class_name: 'PostLike', dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
